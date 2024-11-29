9709e11089b484b42053a2489df595c3a083e85b
7soon66 <68373803+7soon66@users.noreply.github.com>
2024-11-29 16:42:12 +0300

    excersie done finaly

--- app.js
+++ app.js
@@ -1,78 +1,119 @@
 /*-------------------------------- Constants --------------------------------*/
-const squareEls = Array.from(document.getElementsByClassName('sqr'))
-const restartBtn = document.getElementById('restartBtn')
-const messageEl = document.querySelector('#message')
-let spaces = document.querySelectorAll('.board')
-
-console.log(squareEls)
-const board = ['', '', '', '', '', '', '', '', '']
-console.log(board)
-
-/*---------------------------- Variables (state) ----------------------------*/
-
-const turn = 'x'
-const otherTurn = 'o'
-let currentPlayer = turn
+let board = ['', '', '', '', '', '', '', '', '']
+let turn = 'X'
 let winner = false
 let tie = false
+/*---------------------------- Variables (state) ----------------------------*/
+const squareEls = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+const messageEl = document.querySelector('#message')
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8]
+]
 
 /*------------------------ Cached Element References ------------------------*/
+const resetBtnEl = document.querySelector('#reset')
 
 /*-------------------------------- Functions --------------------------------*/
-const startGmae = () => {
-  squareEls.forEach((sequare) =>
-    sequare.addEventListener('click', sequareClicked)
-  )
+const init = () => {
+  board = ['', '', '', '', '', '', '', '', '']
+  turn = 'X'
+  winner = false
+  tie = false
+  render()
+  messageEl.textContent = ''
 }
-const sequareClicked = (el) => {
-  id = el.target.id
-  if (!board[id]) {
-    board[id] = currentPlayer
-    el.target.textContent = currentPlayer
-    if (currentPlayer === turn) {
-      currentPlayer = otherTurn
-    } else {
-      currentPlayer = turn
-    }
-    if (checkForWinner() !== false) {
-      messageEl = `${currentPlayer} has won`
-      let winner = checkForWinner()
-      console.log(winner)
-    }
-  }
+const render = () => {
+  updateBoard()
+  updateMessage()
 }
 
-const checkForWinner = (board) => {
-  const winngCombination = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ]
-  for (let i = 0; i < winngCombination.length; i++) {
-    const [a, b, c] = winngCombination[i]
-    if (
-      board[winngCombination[i][0]] !== '' &&
-      board[winngCombination[i][1]] === board[winngCombination[i][0]] &&
-      board[winngCombination[i][2]] === board[winngCombination[i][1]]
-    ) {
-      console.log(board[a])
-      return board[a]
-    }
-  }
-  // return null
-}
-const restart = () => {
-  board.fill(null)
-  squareEls.forEach((sequare) => {
-    sequare.textContent = ' '
+const updateBoard = () => {
+  board.forEach((square, index) => {
+    document.querySelector(`#square${index}`).textContent = square
   })
-  currentPlayer = turn
 }
-restartBtn.addEventListener('click', restart)
 
-startGmae()
+const updateMessage = () => {
+  if (winner === false && tie === false) {
+    messageEl.textContent = `The turn is for player ${turn}`
+  } else if (winner === false && tie === true) {
+    messageEl.textContent = `The game ended tie!!!`
+  } else {
+    messageEl.textContent = `Congrats ${turn} you won!!!`
+  }
+}
+
+const placePiece = (index) => {
+  board[index] = turn
+}
+
+checkForWinner = () => {
+  winningCombos.forEach((comp) => {
+    if (
+      board[comp[0]] === board[comp[1]] &&
+      board[comp[1]] === board[comp[2]] &&
+      board[comp[0]] !== ''
+    ) {
+      winner = true
+    }
+  })
+}
+
+const checkForTie = () => {
+  let ti = true
+
+  board.forEach((val) => {
+    if (val === '') {
+      ti = false
+    }
+  })
+
+  if (ti === true && winner === false) {
+    tie = true
+  } else {
+    tie = false
+  }
+}
+
+const switchPlayerTurn = () => {
+  if (winner === true) {
+    return
+  }
+  if (turn === 'X') {
+    turn = 'O'
+  } else {
+    turn = 'X'
+  }
+}
+
+const handelClick = (event) => {
+  let squareIndex = event.target.id
+  if (
+    board[squareIndex] === 'X' ||
+    board[squareIndex] === 'O' ||
+    winner === true
+  ) {
+    return
+  }
+  placePiece(squareIndex[6])
+  updateBoard()
+  checkForWinner()
+  checkForTie()
+  switchPlayerTurn()
+  updateMessage()
+}
+/*----------------------------- Event Listeners -----------------------------*/
+squareEls.forEach((square) => {
+  document
+    .querySelector(`#square${square}`)
+    .addEventListener('click', handelClick)
+})
+
+resetBtnEl.addEventListener('click', init)
